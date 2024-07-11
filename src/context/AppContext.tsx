58d82a45bfc219f5a21b13d279a063d21e871a0c
GoodGuyForReal <tdoganay325@gmail.com>
2024-07-11 10:04:57 +0200

    context api updated

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -1,33 +1,9 @@
 import React, { createContext, ReactNode, useContext, useState } from "react";
+import { AppContextType, Order } from "../types/types";
 import { useOrderBook } from "../hooks/useOrderBook"; 
 import { ProductIds } from "../constants/constants";
-import { OrderBookEntry } from "../types/types";
 
-interface Order {
-  orderId: number;
-  orderType: "BUY_LIMIT" | "SELL_LIMIT" | "MARKET_BUY" | "MARKET_SELL";
-  pair: string;
-  price: number;
-  quantity: number;
-  total: number;
-  creationDate: Date;
-  completionDate: Date | null;
-  status: "Pending" | "Filled" | "Canceled";
-}
 
-interface AppContextType {
-    selectedPair: string;
-    balance: number;
-    setBalance: React.Dispatch<React.SetStateAction<number>>;
-    setPair: (pair: string) => void;
-    currentBids: OrderBookEntry[];
-    currentAsks: OrderBookEntry[];
-    currentIndex: number;
-    loading: boolean;
-    orderHistory: Order[];
-    createOrder: (order: Omit<Order, "orderId" | "completionDate" | "status">) => void;
-    completeOrder: (orderId: number) => void;
-  }
 
 const AppContext = createContext<AppContextType | undefined>(undefined);
 
--- src/types/types.ts
+++ src/types/types.ts
@@ -8,3 +8,30 @@ export interface Delta {
   bids?: number[][];
   asks?: number[][];
 }
+
+
+export interface Order {
+  orderId: number;
+  orderType: "BUY_LIMIT" | "SELL_LIMIT" | "MARKET_BUY" | "MARKET_SELL";
+  pair: string;
+  price: number;
+  quantity: number;
+  total: number;
+  creationDate: Date;
+  completionDate: Date | null;
+  status: "Pending" | "Filled" | "Canceled";
+}
+
+export interface AppContextType {
+    selectedPair: string;
+    balance: number;
+    setBalance: React.Dispatch<React.SetStateAction<number>>;
+    setPair: (pair: string) => void;
+    currentBids: OrderBookEntry[];
+    currentAsks: OrderBookEntry[];
+    currentIndex: number;
+    loading: boolean;
+    orderHistory: Order[];
+    createOrder: (order: Omit<Order, "orderId" | "completionDate" | "status">) => void;
+    completeOrder: (orderId: number) => void;
+  }
