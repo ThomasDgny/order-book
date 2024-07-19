e0a812a7dc4f71fcb484c0f8e07416e63a17bd3c
GoodGuyForReal <tdoganay325@gmail.com>
2024-07-19 22:13:08 +0200

    TickerInfo loading state added

--- src/components/home/_components/TickerInfo.tsx
+++ src/components/home/_components/TickerInfo.tsx
@@ -15,22 +15,28 @@ export default function TickerInfo({
 }: TickerInfoProps) {
   return (
     <div className="flex justify-between items-center py-3 px-8 rounded-md">
-      <div>
-        <ProductSelector
-          selectedPair={selectedPair}
-          handlePairChange={handlePairChange}
-        />
-        <h3 className="text-2xl font-bold">{tickerData.markPrice}</h3>
-        <p className="text-sm font-medium text-slate-500">
-          {tickerData.markPrice}
-        </p>
-      </div>
-      <div className="flex gap-10">
-        <InfoCard label="24h change" value={tickerData.change} />
-        <InfoCard label="24h high" value={tickerData.high} />
-        <InfoCard label="24h low" value={tickerData.low} />
-        <InfoCard label="24h volume" value={tickerData.volume} />
-      </div>
+      {tickerData.markPrice ? (
+        <>
+          <div>
+            <ProductSelector
+              selectedPair={selectedPair}
+              handlePairChange={handlePairChange}
+            />
+            <h3 className="text-2xl font-bold">{tickerData.markPrice}</h3>
+            <p className="text-sm font-medium text-slate-500">
+              {tickerData.markPrice}
+            </p>
+          </div>
+          <div className="flex gap-10">
+            <InfoCard label="24h change" value={tickerData.change} />
+            <InfoCard label="24h high" value={tickerData.high} />
+            <InfoCard label="24h low" value={tickerData.low} />
+            <InfoCard label="24h volume" value={tickerData.volume} />
+          </div>
+        </>
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
 }
